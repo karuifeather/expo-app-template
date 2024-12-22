1735cee37ee7123074588d3ddbbbe54218fbee33
Aashaya Aryal <aryalaashaya@gmail.com>
2024-12-21 19:37:27 -0600

    added tailwind, eslint, prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from 'react-native';
+import './style.css';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center bg-fuchsia-600">
+      <Text className="text-red-100 font-bold text-4xl">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
---
+++ src/src.components.ts
@@ -0,0 +1,2 @@
+// Add your non routing src files in /src
+export const variables = true;
